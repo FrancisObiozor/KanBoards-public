e9a7842af29198e70ae9009a062322b6bcbcf524
Francis Obiozor <fobiozor19@gmail.com>
2024-02-04 20:30:20 -0500

    Moved folder location

--- KanBoards/Kanboards/obj/Debug/net6.0/Api.AssemblyInfo.cs
+++ KanBoards/Kanboards/obj/Debug/net6.0/Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+589499da5be87688284b7387ddf091526bace538")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
